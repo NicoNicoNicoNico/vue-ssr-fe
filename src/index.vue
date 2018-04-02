ad58a06ca397a80c5d36fd80f012859f46f0d9d5
jiangshan <jiangshan@hofan.cn>
2018-04-02 19:32:04 +0800

    first push

---
+++ src/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+			Name: <input v-model="name" type="text">
+			<h1>Hello Component</h1>
+			<hello-component :name="name" :initialEnthusiasm="5" />
+			<h1>Hello Decorator Component</h1>
+			<hello-decorator-component :name="name" :initialEnthusiasm="5" />
+			</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import HelloComponent from "./components/Hello.vue";
+import HelloDecoratorComponent from "./components/HelloDecorator.vue";
+
+export default Vue.extend({
+    data() {
+			return {
+				name: "World"
+			}
+    },
+    components: {
+			HelloComponent,
+			HelloDecoratorComponent
+    }
+});
+</script>
+
+<style>
+</style>
